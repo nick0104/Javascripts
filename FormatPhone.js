f2f02bc4f64dd4a98b60c3c42485c0dd124fe0ba
nick0104 <nick0104@hotmail.com>
2015-01-18 10:38:29 -0500

    Took out the first dash after the first 3 digits in the formatted string

--- FormatPhone.js
+++ FormatPhone.js
@@ -6,8 +6,8 @@
 	//  1234567890x124 (10 numerics) followed by "x" followed by 1 to 6 digits for the extension.
 	//
 	// Returns:
-	// 123-456-7890 (for 10 digits string)
-	// 123-456-7890x123 (for 10 digit string, "x", and up to 6 digit extension.
+	// (123)456-7890 (for 10 digits string)
+	// (123)456-7890x123 (for 10 digit string, "x", and up to 6 digit extension.
 	// "" Not a valid phone that adheres to the input.
 
 	function formatPhone (phone)
@@ -19,7 +19,7 @@
 		if (res)
 		{
 			// handle the case of 10 digits and no extension
-			return phone.replace (patt, "("+'$1'+")"+"-"+'$2'+"-"+'$3');
+			return phone.replace (patt, "("+'$1'+")"+'$2'+"-"+'$3');
 		}
 
 		// Lets test if it is 10 digits with an extension as in this example 8008872213x123
@@ -29,7 +29,7 @@
  		res = patt.test(phone);
 		if (res)
 		{
-			return phone.replace (patt,"("+'$1'+")"+"-"+'$2'+"-"+'$3'+'$4'+'$5');
+			return phone.replace (patt,"("+'$1'+")"+'$2'+"-"+'$3'+'$4'+'$5');
 		}
 		// Not a matched phone pattern. Return an emplty string to indicate an error to the caller
 		return "";
